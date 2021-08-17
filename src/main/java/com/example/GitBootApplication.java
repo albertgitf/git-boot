c26675f57fd9f78fcbd321a5e6198049929725a9
Albert <Albert@gmail.com>
2021-08-17 15:42:08 +0800

    Version 1.0.1[热修复Bug]

--- src/main/java/com/example/GitBootApplication.java
+++ src/main/java/com/example/GitBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Version 1.0.0 已弃用
  * Version 1.0.1 已弃用
- * Version 1.0.2 正在启用       Bug1热修复成功
+ * Version 1.0.2 正在启用       Bug1热修复成功       Bug2热修复成功
  */
 @SpringBootApplication
 public class GitBootApplication {
