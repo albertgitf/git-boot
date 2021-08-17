3c54705ee3afab53e2b27c1fdf281fbcee77ffd8
Albert <Albert@gmail.com>
2021-08-17 15:25:13 +0800

    Version 1.0.0

---
+++ src/main/java/com/example/GitBootApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Version 1.0.0 正在启用
+ */
+@SpringBootApplication
+public class GitBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitBootApplication.class, args);
+    }
+
+}
