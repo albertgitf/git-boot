93265fd884005f2caf966bacc3f91f38e005e203
albertgitf <89055858+albertgitf@users.noreply.github.com>
2021-08-17 16:11:55 +0800

    Update GitBootApplication.java
    1.0.4

--- src/main/java/com/example/GitBootApplication.java
+++ src/main/java/com/example/GitBootApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitBootApplication {
 
     public static void main(String[] args) {
+        //在Github上修改过后的代码
         SpringApplication.run(GitBootApplication.class, args);
     }
 
